63510f5d7b5c139c5d1697d8be2697a98ddb0386
goncaloramos11 <goncalo.bras.ramos@gmail.com>
2026-03-27 12:58:20 +0000

    template for education

--- src/Education/Education.jsx
+++ src/Education/Education.jsx
@@ -3,15 +3,16 @@ function Education(){
     return (
     <div className="education">
         <div className="left-side">
-            <div className="school">FCUL</div>
-            <div className="degree">FCUL</div>
-        </div>
-        <div className="right-side">
             <div className="date">
                 <div className="start-date">20/02/2022</div>
+                <span>-</span>
                 <div className="end-date">20/4/2026</div>
             </div>
             <div className="location">Lisbon</div>
+        </div>
+        <div className="right-side">
+            <div className="school">London City University</div>
+            <div className="degree">Bachelors in Economics</div>
         </div>
     </div>
     );
